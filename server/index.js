1729c7928baae3b99ea72cd635fcc538b1d8ba8f
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2021-11-20 16:47:36 +0200

    Add eslint for the server.

--- server/index.js
+++ server/index.js
@@ -8,5 +8,5 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
