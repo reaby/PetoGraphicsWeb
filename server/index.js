ff023c401f82d79b141627d2b275beb1e07d04b4
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2021-11-20 16:22:36 +0200

    Initial commit.

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
