7455799865c00858025854bdc10edd9ec6fff26b
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2021-11-28 01:33:05 +0200

    Change build script to install only production dependencies instead of copying all.

--- build.bat
+++ build.bat
@@ -1,25 +1,14 @@
-echo Building project...
 
-:: Client
-echo Building client...
+:: Build client
 cd client
 call npm install
 call npm run build
-
-:: Server
-echo Building server...
-cd ../server
-call npm install
-call nexe index.js -b -o PetoGraphicsWeb.exe --ico ..\client\public\Icon.ico
-
-:: Dist
 cd ..
-echo Creating distribution...
-rmdir /s /q dist
-Xcopy /E /I server dist
-move client\build dist\client
 
-:: Cleanup
-rmdir /s /q dist\configs
-del dist\.gitignore
-del dist\.eslintrc.json
+:: Create executable
+Xcopy /E /I server dist /EXCLUDE:buildExclude.txt
+move client\build dist\client
+cd dist
+call npm install --only=prod
+call nexe index.js -b -o PetoGraphicsWeb.exe --ico ..\client\public\Icon.ico
+cd ..
