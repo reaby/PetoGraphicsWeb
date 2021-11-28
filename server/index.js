ee07fc7b2faced4adc053bead42fe2a92c25ec97
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2021-11-28 02:40:00 +0200

    Detach app from index.js in preparation for tests.

--- server/index.js
+++ server/index.js
@@ -1,73 +1,7 @@
-import Express from 'express';
-import { WebSocketServer } from 'ws';
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
-import morgan from 'morgan';
-import fs from 'fs';
+import { app, wss } from './app';
 import open from 'open';
-import ApiRouter from './routes/router.js';
 
 const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
-
-if (!fs.existsSync('./configs')) {
-    fs.mkdirSync('./configs');
-}
-
-const app = Express();
-
-// WebSocket server
-const wss = new WebSocketServer({ noServer: true });
-wss.on('connection', (ws) => {
-    ws.id = uuidv4();
-    ws.on('message', (msg) => {
-        const { type, payload } = JSON.parse(msg);
-        switch (type) {
-            case 'update-config':
-                app.locals.setConfig(payload, ws.id);
-                break;
-            default:
-                break;
-        }
-        if (type) {
-            console.log(`Received message => ${type}`);
-        }
-    });
-    ws.send(JSON.stringify({ type: 'config', payload: { project: app.locals.project, config: app.locals.config } }));
-});
-
-wss.broadcastConfig = (ignoreClient = null) => {
-    wss.clients.forEach((client) => {
-        if (client.id !== ignoreClient) {
-            client.send(JSON.stringify({ type: 'config', payload: { project: app.locals.project, config: app.locals.config } }));
-        }
-    });
-};
-
-// Main app
-app.locals.config = [];
-app.locals.setConfig = (config, ignoreClient = null) => {
-    app.locals.config = config;
-    wss.broadcastConfig(ignoreClient);
-};
-app.locals.project = null;
-app.locals.setProject = (project, ignoreClient = null) => {
-    app.locals.project = project;
-    wss.broadcastConfig(ignoreClient);
-};
-
-// Middlewares
-app.use(Express.json());
-app.use(Express.urlencoded({ extended: true }));
-app.use(morgan('tiny'));
-// Routes
-app.use('/api/', ApiRouter);
-app.use('/configs', Express.static(path.join(__dirname, 'configs')));
-app.use('/static', Express.static('C:'));
-app.use(Express.static(path.resolve(__dirname, './client')));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './client', 'index.html'));
-});
 
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 server.on('upgrade', (request, socket, head) => {
